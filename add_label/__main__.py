b19e22e01347775102172898065b17a5141997d2
Paul Luna <paulluna0215@gmail.com>
2023-09-06 17:26:53 -0700

    chore: removed unused imports

--- add_label/__main__.py
+++ add_label/__main__.py
@@ -1,12 +1,9 @@
-import requests
-import json
-
-from github import Github
+from github import Github, GithubException
 # Authentication is defined via github.Auth
 from github import Auth
 
 # using an access token
-auth = Auth.Token("")
+auth = Auth.Token("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")
 
 # First create a Github instance:
 
@@ -20,6 +17,6 @@ g = Github(auth=auth)
 for repo in g.get_user().get_repos():
     if "QuanMol" in repo.full_name:
         try:
-            print(f"Adding label to {repo.full_name}....", repo.create_label("epic: test", "BFDADC", "test label"))
-        except:
-            continue
+            print(f"Adding label to {repo.full_name}....", repo.create_label("redefine-production", "E9BE95", "Issue pertaining to redefine-production eks cluster"))
+        except GithubException as e:
+            print('Label already exists', e)
